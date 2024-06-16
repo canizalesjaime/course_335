44ea42a1b8ce245f5cd03e33cef83787ca673ff8
canizalesjaime <canizales.jaime16@gmail.com>
2024-06-16 14:00:49 -0400

    minor updates

--- class_code/week4/06-17_stl/stl_iterators.cpp
+++ class_code/week4/06-17_stl/stl_iterators.cpp
@@ -9,87 +9,117 @@ smallest value, and using the "?" ternary operator.*/
 #include<vector>
 #include<list>
 #include<unordered_map>
-#include<set>
 #include<limits>
+#include<algorithm>
 
 using namespace std;
 
+
+///////////////////////////////////////////////////////////////////////////////
 void test_vector()
 {
-    /*for(int x: vec) // then show with &
-    {
-        cout<<x<<" ";
-        x=7;
-    }
-    cout<<"printed first time"<<endl;
-
-    for(int x : vec) cout<<x<<" "; 
-
-    cout<<"printed second time"<<endl;
+    //iterators are pointers used in stl for traversing through data structures
+    vector<int> vec({2,4,6,8,10,47,1,5});
+    vector<int>::iterator itr;
+    for (itr = vec.begin(); itr != vec.end(); itr++)
+        cout<<*itr<<" ";
+    cout<<"printed with iterators"<<endl;
 
     int max_val = std::numeric_limits<int>::min();
     cout<<"minimum int value: "<<max_val<<endl;
     for(int x : vec) max_val = (x>max_val)?x:max_val; //ternary operator ?
-
     cout<<"max value in vec "<<max_val<<endl;
 
-    for (auto x: vec) cout<<x<<" ";
-    cout<<"printed third time with auto"<<endl;
-
-     //iterators are pointers used in stl for traversing through data structures
-     vector<int>::iterator itr;
-
-     for (itr = vec.begin(); itr != vec.end(); itr++)
-        cout<<*itr<<" ";
-    cout<<"printed 4th time with iterators"<<endl;*/
-    vector<int> vec({2,4,6,8,10,47,1,5});
     //use cases for iterators insert 
     vec.insert(vec.begin()+3,399);
     for (auto x: vec) cout<<x<<" ";
-    cout<<"printed fifth time with inserted element"<<endl;
+    cout<<"Inserted element 399"<<endl;
 
     vector<int> sub_vec = {3,2,1};
-    vec.insert(vec.begin()+3,sub_vec.begin(),sub_vec.end());
+    vec.insert(vec.begin()+4,sub_vec.begin(),sub_vec.end());
     for (auto x: vec) cout<<x<<" ";
-    cout<<"printed sixth time with inserted sub vector"<<endl;
+    cout<<"Inserted sub vector {3,2,1}"<<endl;
+    
+    //sort vector
+    sort(vec.begin(), vec.end());
+    for (auto x: vec) cout<<x<<" ";
+    cout<<"Sorted Vec"<<endl;
 
-    ///////////////////////////////////////////////////////////////////////////
     //use cases for iterators erase 
     vec.erase(vec.begin()+5);
     for (auto x: vec) cout<<x<<" ";
-    cout<<"printed seventh time with erased element"<<endl;
+    cout<<"erase 5th element"<<endl;
     
     vec.erase(vec.begin()+1,vec.begin()+3);
     for (auto x: vec) cout<<x<<" ";
-    cout<<"printed eighth time with erased sequence"<<endl;
-
-    //delete lasty three elements
-    vec.erase(vec.end()-3,vec.end());
-    for (auto x: vec) cout<<x<<" ";
-    cout<<"printed nineth time with erased sequence"<<endl;
-    
+    cout<<"erase 1st to 3rd elements"<<endl;
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
 void test_list()
 {
     list<double> my_list1({1,2,30,4.2});
 
+    //print list1
+    cout<<"List 1: ";
     for(auto x : my_list1) cout<<x<<" ";
     cout<<endl;
 
+    // Accessing list elements
     //cout<<my_list1[1]<<endl; //not allowed
-
     int i = 0;
     for(auto x : my_list1)
     {
-        if (i == 3) cout<<x;
+        if (i == 3) cout<<"Iterate through list: "<<x<<endl;
         i++;
     } 
+    
+    list<double>::iterator it=my_list1.begin();
+    advance(it, 3);
+    cout<<"Use advance function to access: "<<*it<<endl;
+
+    // insert at index 2, the element 1.618
+    it = my_list1.begin();
+    int end_pos = 2; // try 10
+    for (int i = 0; i < end_pos && it!=my_list1.end(); i++) it++;
+    if(it!=my_list1.end()) my_list1.insert(it,1.618);
+     for(auto x : my_list1) cout<<x<<" ";
     cout<<endl;
 
+    //insert at index 2, the element 5
+    it = my_list1.begin();
+    advance(it, 2);
+    my_list1.insert(it, 5);
+    for(auto x : my_list1) cout<<x<<" ";
+    cout<<endl;
+
+    //erase index 1
+    my_list1.erase(++my_list1.begin());
+    for(auto x : my_list1) cout<<x<<" ";
+    cout<<endl;
+
+    list<double> my_list2({9,8,7,6.2});
+    cout<<"List 2: ";
+    for(auto x : my_list2) cout<<x<<" ";
+    cout<<endl;
+    
+    //merge
+    my_list1.sort();
+    my_list2.sort();
+    my_list1.merge(my_list2);
+    for(auto x : my_list1) cout<<x<<" ";
+    cout<<endl;
+    for(auto x : my_list2) cout<<x<<" "; //empty now
+    cout<<endl;
+
+    //splice
+    
 
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
 void test_unordered_maps()
 {
     unordered_map<int, string> my_map1;
@@ -121,6 +151,5 @@ void test_unordered_maps()
 int main()
 {
    test_list();
-
    return 0;
 }
