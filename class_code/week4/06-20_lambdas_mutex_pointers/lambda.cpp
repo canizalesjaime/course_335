5875e1630728c26db7a83951df763fd2ed27eef9
canizalesjaime <canizales.jaime16@gmail.com>
2024-06-19 17:02:42 -0400

    updated lambda code

--- class_code/week4/06-20_lambdas_mutex_pointers/lambda.cpp
+++ class_code/week4/06-20_lambdas_mutex_pointers/lambda.cpp
@@ -1,12 +1,11 @@
 #include <vector>
 #include <string>
 #include <iostream>
-#include<unordered_map>
-#include<memory>
 #include<algorithm>
 #include<functional>
 
 using namespace std;
+
 
 //void(*func)(int)
 void ForEach(const vector<int>& values, const function<void(int)>& func)
@@ -22,6 +21,9 @@ bool isPalindrome(string s)
     for_each(s.begin(), s.end(),[](char& c){c = tolower(c);});
     
     //remove non-alphanumeric characters
+    //does so by moving all nonalphanumeric character to end of string
+    //then you must delete character from first invalid character located at
+    //it and onwards
     auto it = remove_if(s.begin(), s.end(), 
                         [](char const &c){return !isalnum(c);});
     s.erase(it, s.end());
@@ -35,6 +37,12 @@ bool isPalindrome(string s)
 
 void tempFunc(int value)
 {
+    cout<<"Hi: "<<value<<" ";
+}
+
+
+void tempFunc2(string value)
+{
     cout<<"Hi: "<<value<<endl;
 }
 
@@ -43,16 +51,30 @@ int main()
 {
     typedef void(*func)(int);
     func p = tempFunc;
+    void(*tempFunc2Ptr)(string) = tempFunc2;
     cout<<"Here: ";
     p(3);
-
-    // ) mutable {
+    tempFunc2Ptr("sup");
+ 
     vector<int> values({7,4,8, 10, 81, 3});
     string x = "Hello: ";
-    auto lambda = [x](int value){cout<<x<<value<<endl;};
 
-    
+    // ) mutable { modify capture variables.pass by copy =, pass by reference &
+    auto lambda = [&x](int value){cout<<x<<value<<" ";};
     ForEach(values,lambda);
+    cout<<endl;
     ForEach(values, tempFunc);
+    cout<<endl;
 
+    cout<<isPalindrome("Apple-Sauce")<<endl;
+    cout<<isPalindrome("Race-c-ar-")<<endl;
+
+    sort(values.begin(), values.end());
+    x="";
+    ForEach(values,lambda);
+    cout<<endl;
+
+    sort(values.begin(),values.end(),[](int a, int b){return a > b;});
+    ForEach(values,lambda);
+    return 0;
 }
